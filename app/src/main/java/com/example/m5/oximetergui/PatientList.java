df9402849969165ab927c0f3991969dbc3373bc3
danabeled <abel.d@husky.neu.edu>
2015-01-27 17:06:33 -0500

    Made function to fully create a list of TextViews

--- app/src/main/java/com/example/m5/oximetergui/PatientList.java
+++ app/src/main/java/com/example/m5/oximetergui/PatientList.java
@@ -1,15 +1,23 @@
 package com.example.m5.oximetergui;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Canvas;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 
 public class PatientList extends Activity implements View.OnClickListener {
+
+    RelativeLayout mMainLayout = null;
+    ArrayList<String> mPatientNames = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +25,7 @@ public class PatientList extends Activity implements View.OnClickListener {
         setContentView(R.layout.activity_patient_list);
         View patientListButton = findViewById(R.id.new_patient);
         patientListButton.setOnClickListener(this);
+
     }
 
     static final int NEW_PATIENT_REQUEST = 1;
@@ -37,6 +46,38 @@ public class PatientList extends Activity implements View.OnClickListener {
             ArrayList<String> patientData = data.getStringArrayListExtra("PatientInfo");
             Log.d("PatientList", patientData.get(0));
             Log.d("PatientList", patientData.get(1));
+            String fullName = patientData.get(0) + " " + patientData.get(1);
+            Log.d("PatientList", fullName);
+            mPatientNames.add(fullName);
+            this.CreateTextList(this, mPatientNames, R.id.new_patient);
+        }
+    }
+
+    public void CreateTextList (Context context, ArrayList<String> list, int id) {
+        RelativeLayout layout = (RelativeLayout) findViewById(R.id.relativePatientList);
+
+        int prevTextViewId = 0;
+        for (int i = 0; i < list.size(); i++) {
+            final TextView textView = new TextView(context);
+
+            textView.setText(list.get(i));
+
+            int curTextViewId = prevTextViewId + 1;
+            textView.setId(curTextViewId);
+            final RelativeLayout.LayoutParams params =
+                    new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.FILL_PARENT,
+                            RelativeLayout.LayoutParams.WRAP_CONTENT);
+
+            if (i == 0 && id != -1) {
+                params.addRule(RelativeLayout.BELOW, id);
+            } else {
+                params.addRule(RelativeLayout.BELOW, prevTextViewId);
+            }
+            textView.setLayoutParams(params);
+
+            prevTextViewId = curTextViewId;
+            layout.addView(textView, params);
         }
     }
 }
+
