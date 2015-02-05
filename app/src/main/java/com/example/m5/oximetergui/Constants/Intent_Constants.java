08ffb9d38b4c7be2ec39cc474ebe6fc57d682a44
danabeled <abel.d@husky.neu.edu>
2015-02-05 14:51:26 -0500

    removed finished TODO

--- app/src/main/java/com/example/m5/oximetergui/Constants/Intent_Constants.java
+++ app/src/main/java/com/example/m5/oximetergui/Constants/Intent_Constants.java
@@ -3,7 +3,7 @@ package com.example.m5.oximetergui.Constants;
 /**
  * Created by Hunt on 2/3/2015.
  */
-public class Intent_Constants { // TODO fill this thang out Dan!
+public class Intent_Constants {
     public static String NewPatientInfo = "NewPatientInfo";
     public static String NameToPatient = "FullName";
     public static String ReadingDate = "Date";
