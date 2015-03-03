59949cf32941c4f8af8144518ffd54ad8ef5d915
hunt <hunt.b.graham@gmail.com>
2015-03-02 21:22:50 -0500

    Added in a  hello word for chart data.

--- app/src/main/java/com/example/m5/oximetergui/Activities/PatientInfo.java
+++ app/src/main/java/com/example/m5/oximetergui/Activities/PatientInfo.java
@@ -5,11 +5,22 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.example.m5.oximetergui.Constants.Intent_Constants;
 import com.example.m5.oximetergui.Data_Objects.Patient;
 import com.example.m5.oximetergui.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import lecho.lib.hellocharts.model.Line;
+import lecho.lib.hellocharts.model.LineChartData;
+import lecho.lib.hellocharts.model.PointValue;
+import lecho.lib.hellocharts.model.ValueShape;
+import lecho.lib.hellocharts.util.ChartUtils;
+import lecho.lib.hellocharts.view.LineChartView;
 
 public class PatientInfo extends ActionBarActivity {
 
@@ -27,7 +38,40 @@ public class PatientInfo extends ActionBarActivity {
         Patient patientData = getIntent().getParcelableExtra(Intent_Constants.Patient_To_Edit);
         _name.setText(patientData.FirstName + " " + patientData.LastName);
         _age.setText("Age: " + patientData.DateOfBirth);
-        // populate views based on this.
+
+        addChart();
+    }
+
+    private void addChart()
+    {
+        LineChartView chart = new LineChartView(this);
+        ViewGroup layout = (ViewGroup) findViewById(R.id.graphContainer);
+        layout.addView(chart);
+
+        List<PointValue> values = new ArrayList<PointValue>();
+        values.add(new PointValue(1,2));
+        values.add(new PointValue(2,3));
+        values.add(new PointValue(3,4));
+        values.add(new PointValue(4,5));
+        values.add(new PointValue(5,2));
+        values.add(new PointValue(6,3));
+
+        Line line = new Line(values);
+
+        List<Line> lines = new ArrayList<Line>();
+
+        line.setColor(ChartUtils.COLORS[4]);
+        line.setShape(ValueShape.CIRCLE);
+        //line.setCubic(isCubic);
+        line.setFilled(true);
+        line.setHasLabels(true);
+        //line.setHasLabelsOnlyForSelected(hasLabelForSelected);
+        line.setHasLines(true);
+        line.setHasPoints(true);
+        lines.add(line);
+
+        LineChartData data = new LineChartData(lines);
+        chart.setLineChartData(data);
     }
 
     @Override
@@ -35,11 +79,6 @@ public class PatientInfo extends ActionBarActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_info, menu);
         return true;
-    }
-
-    public void saveClick(View v)
-    {
-        //i.putExtra(Intent_Constants.NewPatientInfo, mPatient);
     }
 
     @Override
