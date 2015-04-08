fc1337c77966a85b311bccd52be739e0d33a2a6f
hunt <hunt.b.graham@gmail.com>
2015-04-08 12:59:21 -0400

    Put in settings control and a sync icon

--- app/src/main/java/com/example/m5/oximetergui/Activities/Settings.java
+++ app/src/main/java/com/example/m5/oximetergui/Activities/Settings.java
@@ -4,15 +4,53 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.example.m5.oximetergui.R;
 
 public class Settings extends ActionBarActivity {
 
+    ArrayAdapter adapter;
+    ArrayAdapter read_adapter;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        InitControls();
+    }
+
+    private void InitControls()
+    {
+        Spinner spinner = (Spinner) findViewById(R.id.baud_spinner);
+
+        this.adapter = ArrayAdapter.createFromResource(this,
+                R.array.planets_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        Spinner rec_spinner = (Spinner) findViewById(R.id.record_rate_spinner);
+
+        this.read_adapter= ArrayAdapter.createFromResource(this,
+                R.array.record_rates_array, android.R.layout.simple_spinner_item);
+        read_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        rec_spinner.setAdapter(read_adapter);
+    }
+
+    private void InitializeSettingsData()
+    {
+        String baudRate = "";
+        try
+        {
+            this.adapter.getPosition(baudRate);
+        }
+        catch (Exception e)
+        {
+            String s = e.getMessage();
+        }
     }
 
 
